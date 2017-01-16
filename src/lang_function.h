f03092c4b4c995a1d8cc9f47c9c88e2eb14bb520
Remi Bauzac <rbauzac@free.fr>
2017-01-16 13:55:46 +0100

    Split function and op

--- src/lang_function.h
+++ src/lang_function.h
@@ -1,37 +1,9 @@
-#ifndef __LANG_OBJECTS_H__
-#define __LANG_OBJECTS_H__
+#ifndef __LANG_FUNCTION_H__
+#define __LANG_FUNCTION_H__
 
 #include "lang_value.h"
-
+#include "lang_op.h"
 struct _htable;
-
-typedef enum {
-  OP_NONE = 0,
-  OP_RETURN,
-  OP_LOAD_CONST,
-  OP_LOAD,
-  OP_ADD,
-  OP_SUB,
-  OP_MUL,
-} opCode;
-
-static char *op2str[] =
-{
-  "NONE",
-  "RETURN",
-  "LOAD_CONST",
-  "LOAD",
-  "ADD"
-  "SUB",
-  "MUL"
-};
-
-#define NUM_OPCODES ((int)OP_RETURN + 1)
-
-typedef struct __attribute__((__packed__)) _operation {
-  uint32_t op;
-  uint32_t a, b, r;
-} operation;
 
 /**
  * Functions
